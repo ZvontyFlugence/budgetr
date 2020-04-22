57628e3edc46b320d5f60e33176dbf5ecea099c8
Z'vonty Flugence <zvontyflugence@gmail.com>
2020-04-22 17:48:34 -0400

    Changed color and disabled alert when no error

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,7 @@ function App() {
   return (
     <Router>
       <BudgetrNavbar />
-      <Alert variant="primary" onClose={alertClose} dismissible>
+      <Alert show={showAlert} variant="primary" onClose={alertClose} dismissible>
         {errorMsg}
       </Alert>
       <Switch>
